51cc0379db0e21d26bb42477cc66a193d1671e8d
Olena Soldatova <lena.soldatova@gmail.com>
2025-04-23 20:55:23 +0300

    Fix GitHub Actions workflow config

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,15 +3,18 @@ import './tests/setup';
 
 export default defineConfig({
     testDir: './tests',
+    retries: 1,
     use: {
         baseURL: 'https://www.fophelp.pro/',
         browserName: 'chromium',
         headless: true,
+        trace: 'on-first-retry',
         screenshot: 'only-on-failure',
         video: 'retain-on-failure'
     },
     reporter: [
         ['list'],
+        ['html'],
         ['allure-playwright']
     ]
 });
